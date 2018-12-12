1ba7b9f934cbbfab87ad5467ef001d35e360bf34
Francissmiguell <xfrancismiguelx@gmail.com>
2018-12-12 17:42:42 -0400

    Last commit

--- public/js/slide.js
+++ public/js/slide.js
@@ -1,18 +1,18 @@
 $("#btn-form").click(function () {
     $('html,body').animate({
         scrollTop: $("#contactForm").offset().top
-    }, 2000);
+    }, 1000);
 });
 
 $("#btn-contacto2").click(function () {
     $('html,body').animate({
         scrollTop: $("#contacto").offset().top
-    }, 2000);
+    }, 1000);
 });
 
 $("#btn-vehiculos").click(function () {
     $('html,body').animate({
         scrollTop: $("#galeria").offset().top
-    }, 2000);
+    }, 1000);
 });
 
